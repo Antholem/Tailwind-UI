dd073e2d12124852e46c91b41a20339f52fa7d21
Antholem <antholemlemmanalo@gmail.com>
2024-01-20 00:22:34 +0800

    Update

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,10 +1,10 @@
 import globalState from "./state";
 
 const theme = () => {
-    const { darkMode } = globalState();
+     const { darkMode } = globalState();
 
     const text = {
-        default: `${darkMode ? 'text-white bg-black' : 'text-black bg-white'}`,
+        default: 'text-black',
     };
 
     return {
