b941738c43beb9c58b223a532666555dad9391fc
Antholem <antholemlemmanalo@gmail.com>
2024-01-20 11:34:05 +0800

    Update

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,19 +1,13 @@
 import globalState from "@/app/state";
 
-const theme = () => {
-    const { darkMode } = globalState();
-
-    const colors = {
-        textDefault: darkMode ? 'text-white' : 'text-black',
-        textGray: darkMode ? 'text-gray-300' : 'text-gray-500',
-        textBlue: darkMode ? 'text-blue-300' : 'text-blue-600',
-        textRed: darkMode ? 'text-red-300' : 'text-red-600',
-        textOrange: darkMode ? 'text-orange-300' : 'text-orange-600',
-        textYellow: darkMode ? 'text-yellow-200' : 'text-yellow-400',
-        textGreen: darkMode ? 'text-green-300' : 'text-green-600',
-    };
-
-    return colors;
-}
+const theme = {
+    textDefault: globalState.getState().darkMode ? 'text-white' : 'text-black',
+    textGray: globalState.getState().darkMode ? 'text-gray-300' : 'text-gray-500',
+    textBlue: globalState.getState().darkMode ? 'text-blue-300' : 'text-blue-600',
+    textRed: globalState.getState().darkMode ? 'text-red-300' : 'text-red-600',
+    textOrange: globalState.getState().darkMode ? 'text-orange-300' : 'text-orange-600',
+    textYellow: globalState.getState().darkMode ? 'text-yellow-200' : 'text-yellow-400',
+    textGreen: globalState.getState().darkMode ? 'text-green-300' : 'text-green-600',
+};
 
 export default theme;
--- src/app/views/components/content/page.tsx
+++ src/app/views/components/content/page.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import theme from '@/app/theme';
 
 const Content = () => {
-  const { textOrange } = theme();
 
   return (
     <>
-      <button className={`${textOrange} p-4`}>
-        sas
+      <button className={`${theme.textOrange} text-6xl font-black`}>
+        SAMPLE COLOR
       </button>
     </>
   );
