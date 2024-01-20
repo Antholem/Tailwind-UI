31f35de5f332ffced35d1d65955dc1d39b244a22
Antholem <antholemlemmanalo@gmail.com>
2024-01-20 11:19:52 +0800

    Update

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -3,25 +3,17 @@ import globalState from "@/app/state";
 const theme = () => {
     const { darkMode } = globalState();
 
-    const text_default = darkMode ? 'text-white' : 'text-black';
-    const text_gray = darkMode ? 'text-gray-300' : 'text-gray-500';
-    const text_blue = darkMode ? 'text-blue-300' : 'text-blue-600';
-    const text_red = darkMode ? 'text-red-300' : 'text-red-600';
-    const text_orange = darkMode ? 'text-orange-300' : 'text-orange-600';
-    const text_yellow = darkMode ? 'text-yellow-200' : 'text-yellow-400';
-    const text_green = darkMode ? 'text-green-300' : 'text-green-600';
-
-    return {
-        text: {
-            text_default,
-            text_gray,
-            text_blue,
-            text_red,
-            text_orange,
-            text_yellow,
-            text_green,
-        }
+    const colors = {
+        textDefault: darkMode ? 'text-white' : 'text-black',
+        textGray: darkMode ? 'text-gray-300' : 'text-gray-500',
+        textBlue: darkMode ? 'text-blue-300' : 'text-blue-600',
+        textRed: darkMode ? 'text-red-300' : 'text-red-600',
+        textOrange: darkMode ? 'text-orange-300' : 'text-orange-600',
+        textYellow: darkMode ? 'text-yellow-200' : 'text-yellow-400',
+        textGreen: darkMode ? 'text-green-300' : 'text-green-600',
     };
+
+    return colors;
 }
 
 export default theme;
--- src/app/views/components/content/page.tsx
+++ src/app/views/components/content/page.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import theme from '@/app/theme';
 
 const Content = () => {
-  const { text } = theme();
+  const { textOrange } = theme();
 
   return (
     <>
-      <button className={`${text.text_red} p-4`}>
+      <button className={`${textOrange} p-4`}>
         sas
       </button>
     </>
