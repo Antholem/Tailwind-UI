69e3d96d344d62a258cc07f46c07a53aac2be0e4
Antholem <antholemlemmanalo@gmail.com>
2024-01-30 19:21:14 +0800

    Jotai Setup

--- src/app/(views)/home/page.tsx
+++ src/app/(views)/home/page.tsx
@@ -1,10 +1,8 @@
-import { useDarkMode } from '@/app/store';
 import React from 'react';
 // import theme from '@/app/theme';
 
 const Home = () => {
   // const { text } = theme();
-  const { darkMode } = useDarkMode()
 
   return (
     <>
@@ -12,7 +10,7 @@ const Home = () => {
         BUTTON
       </button> */}
       <h1>
-        Home Page {darkMode ? "dark" : "light"}
+        Home Page
       </h1>
     </>
   );
