d9b477a26ee735e6069d12f8f3891881a92b1077
Antholem <antholemlemmanalo@gmail.com>
2024-02-08 22:43:43 +0800

    Footer Component

--- src/app/_layouts/footer/index.tsx
+++ src/app/_layouts/footer/index.tsx
@@ -6,6 +6,25 @@ const Footer = () => {
     const { darkMode } = globalState();
     const currentYear = new Date().getFullYear();
 
+    const SocialIcons = [
+        {
+            id: 'twitter',
+            icon: <FaTwitter />,
+        },
+        {
+            id: 'github',
+            icon: <FaGithub />,
+        },
+        {
+            id: 'linkedin',
+            icon: <FaLinkedin />,
+        },
+        {
+            id: 'youtube',
+            icon: <FaYoutube />,
+        },
+    ];
+
     return (
         <div className='py-2'>
             <div className='mb-10'>
@@ -19,9 +38,13 @@ const Footer = () => {
                     </span>
                 </div>
                 <div className='flex flex-row space-x-6'>
-                    <div className={`${darkMode ? 'text-gray-200' : 'text-gray-400'} cursor-pointer`}>
-                        <FaTwitter />
-                    </div>
+                    {SocialIcons.map((item) => (
+                        <div key={item.id}>
+                            <div id={item.id} className={`${darkMode ? 'text-gray-200' : 'text-gray-400'} cursor-pointer`}>
+                                {item.icon}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
