7f4a9bba16fd86f65012a3571ce3e4432ecc6cfa
Antholem <antholemlemmanalo@gmail.com>
2024-02-08 16:31:34 +0800

    Highlighter for Rightsidebar and Footer Components

--- src/app/(views)/button/page.tsx
+++ src/app/(views)/button/page.tsx
@@ -18,34 +18,34 @@ const Button = () => {
     const { darkMode } = globalState();
     const scrollRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (!scrollRef.current) return;
-            const viewportHeight = scrollRef.current.clientHeight;
-            const scrollPosition = scrollRef.current.scrollTop; // Calculate scroll position relative to the center of the viewport
-            const buttonItems = scrollRef.current.getElementsByClassName('scroll-item');
-            for (let i = 0; i < buttonItems.length; i++) {
-                const item = buttonItems[i] as HTMLElement;
-                const itemHeight = item.clientHeight;
-                const offsetTop = item.offsetTop - viewportHeight / 2 + itemHeight / 2; // Adjust offsetTop to center of viewport
-                const offsetBottom = offsetTop + itemHeight;
-                if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
-                    setScrollLocation(item.id);
-                    break;
-                }
-            }
-        };
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //         if (!scrollRef.current) return;
+    //         const viewportHeight = scrollRef.current.clientHeight;
+    //         const scrollPosition = scrollRef.current.scrollTop; // Calculate scroll position relative to the center of the viewport
+    //         const buttonItems = scrollRef.current.getElementsByClassName('scroll-item');
+    //         for (let i = 0; i < buttonItems.length; i++) {
+    //             const item = buttonItems[i] as HTMLElement;
+    //             const itemHeight = item.clientHeight;
+    //             const offsetTop = item.offsetTop - viewportHeight / 2 + itemHeight / 2; // Adjust offsetTop to center of viewport
+    //             const offsetBottom = offsetTop + itemHeight;
+    //             if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+    //                 setScrollLocation(item.id);
+    //                 break;
+    //             }
+    //         }
+    //     };
 
-        if (scrollRef.current) {
-            scrollRef.current.addEventListener('scroll', handleScroll);
-        }
+    //     if (scrollRef.current) {
+    //         scrollRef.current.addEventListener('scroll', handleScroll);
+    //     }
 
-        return () => {
-            if (scrollRef.current) {
-                scrollRef.current.removeEventListener('scroll', handleScroll);
-            }
-        };
-    }, []);
+    //     return () => {
+    //         if (scrollRef.current) {
+    //             scrollRef.current.removeEventListener('scroll', handleScroll);
+    //         }
+    //     };
+    // }, []);
 
     return (
         <Fragment>
