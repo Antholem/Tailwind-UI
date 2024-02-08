6909b6286c48069af8f01d142f6de33a1bae26c0
Antholem <antholemlemmanalo@gmail.com>
2024-02-08 22:50:53 +0800

    Smooth Scroll

--- src/app/_layouts/navigation/rightsidebar/index.tsx
+++ src/app/_layouts/navigation/rightsidebar/index.tsx
@@ -13,22 +13,28 @@ type RightSideBarProps = {
 const RightSideBar: React.FC<RightSideBarProps> = ({ map }) => {
     const { darkMode } = globalState();
 
+    const handleClick = (id: string) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <div className='px-2'>
-            {map.map((component) => (
-                <div key={component.id} >
-                    <ul>
-                        <li
-                            key={component.id}
-                            className={`my-2 px-2 py-1 text-sm font-medium`}
-                        >
-                            <a href={`#${component.id}`}>
-                                {component.title}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            ))}
+            <ul>
+                {map.map((component) => (
+                    <li
+                        key={component.id}
+                        className={`my-2 px-2 py-1 text-sm`}
+                        onClick={() => handleClick(component.id)}
+                    >
+                        <a href={`#${component.id}`} onClick={(e) => e.preventDefault()}>
+                            {component.title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
