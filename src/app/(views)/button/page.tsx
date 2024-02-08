873a27691d99a83d966e84179aa95c2027c68b4f
Antholem <antholemlemmanalo@gmail.com>
2024-02-08 17:50:40 +0800

    Highlighter for Rightsidebar and Footer Components

--- src/app/(views)/button/page.tsx
+++ src/app/(views)/button/page.tsx
@@ -9,47 +9,83 @@ import Footer from '@/app/_layouts/footer';
 
 const Button = () => {
     const [lastCopied, setLastCopied] = useState<number | null>(null);
-    // const [scrollLocation, setScrollLocation] = useState<string>('');
+    const [scrollLocation, setScrollLocation] = useState<string>('');
 
     const handleCopy = (index: number) => {
         setLastCopied(index);
     };
 
     const { darkMode } = globalState();
-    // const scrollRef = useRef<HTMLDivElement>(null);
+    const scrollRef = useRef<HTMLDivElement>(null);
 
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         if (!scrollRef.current) return;
-    //         const viewportHeight = scrollRef.current.clientHeight;
-    //         const scrollPosition = scrollRef.current.scrollTop; // Calculate scroll position relative to the center of the viewport
-    //         const buttonItems = scrollRef.current.getElementsByClassName('scroll-item');
-    //         for (let i = 0; i < buttonItems.length; i++) {
-    //             const item = buttonItems[i] as HTMLElement;
-    //             const itemHeight = item.clientHeight;
-    //             const offsetTop = item.offsetTop - viewportHeight / 2 + itemHeight / 2; // Adjust offsetTop to center of viewport
-    //             const offsetBottom = offsetTop + itemHeight;
-    //             if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
-    //                 setScrollLocation(item.id);
-    //                 break;
-    //             }
-    //         }
-    //     };
+    useEffect(() => {
+        const handleScroll = () => {
+            if (!scrollRef.current) return;
+            const viewportHeight = scrollRef.current.clientHeight;
+            const scrollPosition = scrollRef.current.scrollTop; // Calculate scroll position relative to the center of the viewport
+            const buttonItems = scrollRef.current.getElementsByClassName('scroll-item');
+            for (let i = 0; i < buttonItems.length; i++) {
+                const item = buttonItems[i] as HTMLElement;
+                const itemHeight = item.clientHeight;
+                const offsetTop = item.offsetTop - viewportHeight / 2 + itemHeight / 2; // Adjust offsetTop to center of viewport
+                const offsetBottom = offsetTop + itemHeight;
+                if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+                    setScrollLocation(item.id);
+                    break;
+                }
+            }
+        };
 
-    //     if (scrollRef.current) {
-    //         scrollRef.current.addEventListener('scroll', handleScroll);
-    //     }
+        if (scrollRef.current) {
+            scrollRef.current.addEventListener('scroll', handleScroll);
+        }
 
-    //     return () => {
-    //         if (scrollRef.current) {
-    //             scrollRef.current.removeEventListener('scroll', handleScroll);
-    //         }
-    //     };
-    // }, []);
+        return () => {
+            if (scrollRef.current) {
+                scrollRef.current.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
 
     return (
         <Fragment>
-           
+            <div className='flex flex-1 flex-col space-y-6 overflow-y-auto px-6 md:px-10 py-10' ref={scrollRef}>
+                <div>
+                    <Header
+                        title={ButtonList.ButtonHeader.title}
+                        description={ButtonList.ButtonHeader.description}
+                    />
+                </div>
+                <div>
+                    {ButtonList.ButtonItems.map((item) => (
+                        <Body
+                            id={item.id}
+                            key={item.id}
+                            style='flex flex-col space-y-4 py-6 scroll-item'
+                            title={item.title}
+                            description={item.description}
+                            component={darkMode ? item.component.dark : item.component.light}
+                            clipboard={
+                                <Clipboard
+                                    sourceCode={item.source}
+                                    header={item.clipboard}
+                                    onCopy={() => handleCopy(item.sourceNo)}
+                                    isCheck={lastCopied === item.sourceNo}
+                                />
+                            }
+                        />
+                    ))}
+                </div>
+                <div>
+                    <Footer />
+                </div>
+            </div>
+            <div className='overflow-y-auto hidden md:inline w-[20%]'>
+                <RightSideBar 
+                    map={ButtonList.ButtonItems}
+                    highlight={scrollLocation}
+                />
+            </div>
         </Fragment>
     );
 }
--- src/app/_layouts/footer/index.tsx
+++ src/app/_layouts/footer/index.tsx
@@ -1,16 +1,16 @@
 import globalState from '@/app/state';
-import React from 'react';
-import { FaTwitter, FaGithub, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import React from 'react'
+import { FaTwitter, FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     const { darkMode } = globalState();
     const currentYear = new Date().getFullYear();
 
-    // const socialIcons = [<FaTwitter />, <FaGithub />, <FaLinkedin />, <FaYoutube />];
+    const socialIcons = [<FaTwitter />, <FaGithub />, <FaLinkedin />, <FaYoutube />]
 
     return (
         <div className='py-2'>
-            {/* <div className='mb-10'>
+            <div className='mb-10'>
                 <hr className={`${darkMode ? 'border-gray-600' : 'border-gray-300'}`} />
             </div>
             <div className='flex flex-col space-y-6 justify-center items-center'>
@@ -29,9 +29,9 @@ const Footer = () => {
                         </div>
                     ))}
                 </div>
-            </div> */}
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Footer;
+export default Footer
