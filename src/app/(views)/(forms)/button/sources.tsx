dc19a764be23409683ab16b31cc00d0d9a0cbfe5
Antholem <antholemlemmanalo@gmail.com>
2024-02-10 09:50:20 +0800

    Getting Started Folder

--- src/app/(views)/(forms)/button/sources.tsx
+++ src/app/(views)/(forms)/button/sources.tsx
@@ -23,4 +23,4 @@ const ButtonSourceLight = () => {
 export default {
     ButtonSourceDark,
     ButtonSourceLight
-}
+};
--- src/app/(views)/index.tsx
+++ src/app/(views)/index.tsx
@@ -1,6 +1,6 @@
 import Button from '@/app/(views)/(forms)/button/page';
 import ButtonList from '@/app/(views)/(forms)/button/list';
-import Home from '@/app/(views)/home/page';
+import Home from '@/app/(views)/(getting-started)/home/page';
 
 export {
     Button,
