708182d8bab34f3b55f37c7aee39b87f9a2b73a4
Antholem <antholemlemmanalo@gmail.com>
2024-02-23 21:09:58 +0800

    Forms Component

---
+++ src/app/(views)/(forms)/icon-button/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import { Fragment as Wrap } from '@/app/_layouts';
+
+const Checkbox = () => {
+    return (
+        <Wrap.Body>
+            <div className='text-3xl font-bold'>
+                Icon Button
+            </div>
+        </Wrap.Body>
+    )
+}
+
+export default Checkbox
