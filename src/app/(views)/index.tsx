2160c8f040f34313eb741634848c06ecb89e48f0
Antholem <antholemlemmanalo@gmail.com>
2024-03-04 01:02:30 +0800

    Landing Page

--- src/app/(views)/index.tsx
+++ src/app/(views)/index.tsx
@@ -2,10 +2,8 @@ import Button from '@/app/(views)/(forms)/button/page';
 import ButtonList from '@/app/(views)/(forms)/button/list';
 import Installation from '@/app/(views)/(getting-started)/installation/page';
 import InstallationList from '@/app/(views)/(getting-started)/installation/list';
-import Home from '@/app/(views)/home/page';
 
 export {
-    Home,
     Button,
     ButtonList, 
     Installation,
